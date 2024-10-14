e160a57fb3d8d49ceea5663e0dd9d149c54280c1
Luca Babboni <luca.babboni2@studio.unibo.it>
2024-10-14 14:29:09 +0200

    code refactoring

--- run_model.py
+++ run_model.py
@@ -1,10 +1,22 @@
 import os
+import json
 import asyncio
 import argparse
 from datetime import timedelta
 from minizinc import Instance, Model, Solver
 
 #python run_model.py --method cp --model Model_A.mzn --instance 1 --solver chuffed
+
+BASE_PATH = os.getcwd()
+
+def define_instances_num(instances): 
+
+    if instances == "all":
+        instances =  [i for i in range(1,22)] 
+    else:
+        instances = instances.split(",")
+        instances = [int(i) for i in instances]
+    return instances
 
 def print_configuration(instance, model, method): 
     print(f"solving instance: {instance}, model: {model}, method: {method}")
@@ -34,52 +46,7 @@ def extract_data_from_dat(instance_path, verbose=True):
     return num_vehicles, num_clients, vehicles_capacity, packages_size, distances
 
 
-async def print_intermediate_solutions(instance, timeout=300):
-    async for result in instance.solutions(intermediate_solutions=True, timeout=timeout):
-        print("Intermediate Solution:")
-        print(result.solution)
-        
-        print("Statistics:")
-        print(result.statistics)
-
-
-def main():
-    parser = argparse.ArgumentParser(description="Script that takes method, model, and instance as input.")
-    parser.add_argument('--method', type=str, required=True, help='The method to use')
-    parser.add_argument('--model', type=str, required=True, help='The model to use')
-    parser.add_argument('--instance', type=int, required=True, help='The instance to use')
-    parser.add_argument('--solver', type=str, required=True, help='The solver to use')
-    parser.add_argument('--timeout', type=int, required=True, help='The timeout expressed in seconds', default=300)
-
-
-    args = parser.parse_args()
-    model_name = args.model
-    instance_num = args.instance
-    method = args.method
-    solver_id = args.solver
-    timeout_time = args.timeout
-
-
-    print_configuration(args.instance, args.model, args.method)
-
-    base_path = os.getcwd()
-    model_path = os.path.join(base_path, "CP", "CP_Model", model_name)
-
-
-    instance_file_name = (f"inst{instance_num}" if instance_num >= 10 else f"inst0{instance_num}") + ".dat"
-    instance_path = os.path.join(base_path, "Instances", instance_file_name)
-
-    #check if the files exists
-    if not os.path.isfile(model_path):
-        print(f"The model file can't be found. \nGiven model_ path: {model_path}\nCheck that the file exists")
-        exit()
-    if not os.path.isfile(instance_path):
-        print(f"The instance file can't be found. \nGiven instance_path: {instance_path}\nCheck that the file exists")
-        exit()
-
-    #extract the data from the dat file
-    num_vehicles, num_clients, vehicles_capacity, packages_size, distances = extract_data_from_dat(instance_path)
-
+def solve_instance(model_path, solver_id, num_vehicles, num_clients, vehicles_capacity, packages_size, distances, timeout_time):
     #instanciate the model
     model = Model(model_path)
     solver = Solver.lookup(solver_id)
@@ -94,9 +61,65 @@ def main():
 
     #solve the problem
     timeout = timedelta(seconds=timeout_time)
-    #result = instance.solve(timeout=timeout)
     asyncio.run(print_intermediate_solutions(instance, timeout))
 
+def get_cp_model_path(model_name):
+
+    cp_model_path = os.path.join(BASE_PATH, "CP", "CP_Model", model_name)
+    #check if the files exists
+    if not os.path.isfile(cp_model_path):
+        print(f"The model file can't be found. \nGiven model_ path: {cp_model_path}\nCheck that the file exists")
+        exit()
+
+    return cp_model_path
+
+
+async def print_intermediate_solutions(instance, timeout=300):
+    async for result in instance.solutions(intermediate_solutions=True, timeout=timeout):
+        print(f"\nIntermediate Solution:")
+        print(result.solution)
+        
+    return result.statistics
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Script that takes method, model, and instance as input.")
+    parser.add_argument('--method',     type=str, required=True, default="CP",      help='The method to use')
+    parser.add_argument('--model',      type=str, required=True, default="Model_A", help='The model to use')
+    parser.add_argument('--instance',   type=str, required=True, default="1",         help='The instances to solve')
+    parser.add_argument('--solver',     type=str, required=True, default="gecode",  help='The solver to use')
+    parser.add_argument('--timeout',    type=int, required=True, default=300,       help='The timeout expressed in seconds')
+
+
+    args = parser.parse_args()
+    model_name = args.model
+    method = args.method
+    solver_id = args.solver
+    timeout_time = args.timeout
+    instances = define_instances_num(args.instance)
+    
+    print_configuration(instances, args.model, args.method)
+
+        #select the model path depending on the selected method
+    if method == "CP":
+        model_path = get_cp_model_path(model_name)
+    else: 
+        print("not implemented yet XD")
+        exit()
+
+    for instance_n in instances: 
+        instance_file_name = (f"inst{instance_n}" if instance_n >= 10 else f"inst0{instance_n}") + ".dat"
+        instance_path = os.path.join(BASE_PATH, "Instances", instance_file_name)
+
+        if not os.path.isfile(instance_path):
+            print(f"The instance file can't be found. \nGiven instance_path: {instance_path}\nCheck that the file exists")
+            exit()
+
+        #extract the data from the dat file
+        num_vehicles, num_clients, vehicles_capacity, packages_size, distances = extract_data_from_dat(instance_path)
+
+
+        sol_stat = solve_instance(model_path, solver_id, num_vehicles, num_clients, vehicles_capacity, packages_size, distances, timeout_time)
 
 if __name__ == "__main__":
     main()
