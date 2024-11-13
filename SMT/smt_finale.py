381b7f91c3b7c45777832c249c605a562d378177
luca28 <luca.tedeschini00@gmail.com>
2024-11-13 18:12:59 +0100

    SMT working

--- SMT/smt_finale.py
+++ SMT/smt_finale.py
@@ -1,6 +1,28 @@
 import subprocess
+import json
 
-try:
-    subprocess.call(["python3", "smt_subprocess.py", "7"], timeout=10)
-except subprocess.TimeoutExpired:
-    print("TIMEOUT")
+for i in range(1,21):
+    flag = False
+    try:
+        subprocess.call(["python3", "smt_subprocess.py", str(i)], timeout=300)
+    except subprocess.TimeoutExpired:
+        print("TIMEOUT: ",i)
+        flag = True
+
+    if flag:
+        result_dict = {
+            "time" : 300,
+            "optimal" : False,
+            "obj" : 0,
+            "sol" : []
+        }
+        with open(f"res/res_{i}.json","w") as f:
+            json.dump(result_dict, f)
+    else:
+        with open("tmp_output.json", "r") as f:
+            file = f.readlines()[0]
+
+        print(f"Instance {i}: {file}")
+
+        with open(f"res/res_{i}.json","w") as f:
+            f.write(file)
