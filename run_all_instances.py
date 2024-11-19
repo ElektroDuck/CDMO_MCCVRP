e4fd1987b5237e220a9aba0ae4942c69940a4630
luca28 <luca.tedeschini00@gmail.com>
2024-11-19 17:51:32 +0100

    Changed logic

--- run_all_instances.py
+++ run_all_instances.py
@@ -1,28 +1,9 @@
 import subprocess
+import json
 #python3 main.py --method SMT --model gurobi --solver gecode --instance 1-21  --timeout 300 --update_json True
-MODELS = {
-    "MIP" : {
-        "Models" : {
-            "gurobi" : [],
-            "minizinc" : [
-                "gecode"
-            ]
-        }
-    },
-    "CP" : {
-        "Models" : {
-            "Model_A_gc_corrected_changedoutput.mzn" : [
-                "gecode"
-            ]
-        }
-    },
-    "SMT" : {
-        "Models" : {
-            "SMT" : []
-        }
-    },
-    "instances" : "1-10"
-}
+
+with open("all_inst.json", "r") as f:
+    MODELS = json.load(f)
 
 if __name__ == "__main__":
     for encoding, models in MODELS.items():
